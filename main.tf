cb35d437dc6fb559cfe0502e62846a7c87aeeaeb
sowji <jujjurisowjanya@gmail.com>
2024-01-09 20:28:11 +0530

    new

--- main.tf
+++ main.tf
@@ -4,9 +4,66 @@ resource "aws_vpc" "main" {
 
   tags = merge(
     var.common_tags,
-    var.vpc_tags
+    var.vpc_tags,
     {
-        Name = "${var.project_name}-${var.environment}"
+        Name = local.name
     }
   )
   }
+  resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.common_tags,
+    var.igw_tags,
+    {
+      Name = local.name
+    }
+  )
+    }
+    data "aws_availability_zones" "azs" {
+      state = "available"
+}
+resource "aws_subnet" "public" {
+    count = length(var.public_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.public_subnets_tags,
+    {
+        Name = "${local.name}-public-${local.az_names[count.index]}"
+    }
+  )
+  }
+resource "aws_subnet" "private" {
+    count = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.private_subnets_tags,
+    {
+        Name = "${local.name}-private-${local.az_names[count.index]}"
+    }
+  )
+  }
+  resource "aws_subnet" "database" {
+    count = length(var.database_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnets_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+    var.common_tags,
+    var.database_subnets_tags,
+    {
+        Name = "${local.name}-database-${local.az_names[count.index]}"
+    }
+  )
+  }
+
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable  "vpc_cidr" {
+variable  "cidr" {
   type        = string
   default     = "10.0.0.0/16"
   }
@@ -14,3 +14,47 @@ variable  "vpc_cidr" {
   type        = map
   default     = {}
   } 
+ variable "project_name" {
+  type = string
+ }
+
+ variable "environment" {
+  type = string
+  }
+ variable "igw_tags" {
+    type = map
+    default = {}
+ }
+ variable "public_subnets_tags" {
+    
+    default = {}
+}
+ variable "public_subnets_cidr" {
+    type  = list
+    validation {
+        condition = length(var.public_subnets_cidr) == 2
+        error_message = "please give 2 public valid subnet cidr"
+    }
+ }
+ variable "private_subnets_tags" {
+    
+    default = {}
+}
+ variable "private_subnets_cidr" {
+    type  = list
+    validation {
+        condition = length(var.private_subnets_cidr) == 2
+        error_message = "please give 2 private valid subnet cidr"
+    }
+ }
+ variable "database_subnets_tags" {
+    
+    default = {}
+}
+ variable "database_subnets_cidr" {
+    type  = list
+    validation {
+        condition = length(var.database_subnets_cidr) == 2
+        error_message = "please give 2 database valid subnet cidr"
+    }
+ }
