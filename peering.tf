430290aac84d53c1ed4bf30e38eda7d724ffb90c
sowji <jujjurisowjanya@gmail.com>
2024-02-29 22:15:41 +0530

    new

--- peering.tf
+++ peering.tf
@@ -15,7 +15,7 @@ resource "aws_vpc_peering_connection" "peering" {
 resource "aws_route" "acceptor_peering" {
   count = var.is_peering_required  && var.acceptor_vpc_id == "" ? 1 : 0
   route_table_id = data.aws_route_table.default.id
-  destination_cidr_block = var.cidr
+  destination_cidr_block = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
  
